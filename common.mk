20eb3f2b332ccdde244f102e3fa44d86efe7291e
ghsr <ghsr92@yandex.ru>
2016-07-09 10:27:16 +0600

    galaxys2plus: Add Backup Services whitelist

--- common.mk
+++ common.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/backup.xml:system/etc/sysconfig/backup.xml
+
 # System properties
 $(call inherit-product, device/samsung/galaxys2plus-common/system_prop.mk)
 
