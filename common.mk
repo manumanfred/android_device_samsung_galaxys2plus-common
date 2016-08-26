5d2ad5c4eeecd4c0a87144317cec12e8e70b44af
manumanfred <carlosmanuel_leemet@msn.com>
2016-08-26 13:41:46 +0300

    Add verified_boot config

--- common.mk
+++ common.mk
@@ -90,7 +90,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.verified_boot.xml:system/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
 
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
