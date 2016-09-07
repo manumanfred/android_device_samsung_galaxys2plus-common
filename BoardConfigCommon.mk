1eba576939e843f34915c81d6cd45187cdf8cbd3
manumanfred <carlosmanuel_leemet@msn.com>
2016-09-07 17:32:57 +0300

    BoardConfig: fix typo. Probably fixes BT...

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,7 +81,7 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/galaxys2plus-common/include
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_CUSTOM_BT_CONFIGF := device/samsung/galaxys2plus-common/bluetooth/libbt_vndcfg.txt
+BOARD_CUSTOM_BT_CONFIG := device/samsung/galaxys2plus-common/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/galaxys2plus-common/bluetooth
 
 # Connectivity - Wi-Fi
